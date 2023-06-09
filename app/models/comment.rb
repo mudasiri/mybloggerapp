dfc2c7a6d708cf76143e9304a72666ab0bdf810e
Issah Mudasir <danladi496@gmail.com>
2023-06-09 21:07:52 +0000

    Added-postcounter-3-most-recent-posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :post
+
+    after_create :increment_user_posts_counter
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def increment_user_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
