f79b1d7e9a7a77a593f884a2fd0343284db8f625
Issah Mudasir <danladi496@gmail.com>
2023-06-09 21:35:58 +0000

    Fixed-linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_inter: true, greater_than_or_equal_to: 0 }
-  
+
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
