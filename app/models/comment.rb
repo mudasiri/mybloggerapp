1bb19d33d80afb324d9cdbf605250b59ea25e2c8
Issah Mudasir <danladi496@gmail.com>
2023-06-09 21:09:11 +0000

    Update-comments-counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,15 +2,11 @@ class Comment < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :post
 
-    after_create :increment_user_posts_counter
-
-  def five_most_recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
+    after_create :increment_post_comments_counter
 
   private
 
-  def increment_user_posts_counter
-    author.increment!(:posts_counter)
+  def increment_post_comments_counter
+    post.increment!(:comments_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  after_create :increment_user_posts_counter
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def increment_user_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
