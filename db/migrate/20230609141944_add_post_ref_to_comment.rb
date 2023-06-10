1624833b678088faabbadbc45c4dcfce0ec0cc65
Issah Mudasir <danladi496@gmail.com>
2023-06-10 02:12:29 +0000

    Updated-Migrations-to-fix-minor-errorz-with-test

--- db/migrate/20230609141944_add_post_ref_to_comment.rb
+++ db/migrate/20230609141944_add_post_ref_to_comment.rb
@@ -2,6 +2,6 @@
 
 class AddPostRefToComment < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :post_id, null: false, foreign_key: true
+    add_reference :comments, :posts, null: false, foreign_key: true
   end
 end
