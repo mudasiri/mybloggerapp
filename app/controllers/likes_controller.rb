27d40ca2001d9299f0d8318a1c1bfd4d5b27c4a2
Issah Mudasir <danladi496@gmail.com>
2023-06-16 03:15:33 +0000

    Added-methods-for-likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,15 @@
 class LikesController < ApplicationController
-  def index
+  def create
+    @post = Post.find(params[:post_id])
+    @like = current_user.likes.new
+    @like.post = @post
+
+    if @like.save
+      flash[:success] = 'Post liked successfully'
+      redirect_to user_post_path(@post.author_id, @post)
+    else
+      flash[:error] = 'Error: Post could not be liked'
+      render :new
+    end
   end
 end
