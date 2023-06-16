4de5476c20a4ae3a3e22a23b93cf722bcc063cf0
Issah Mudasir <danladi496@gmail.com>
2023-06-16 03:15:00 +0000

    Added-methods-for-comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,26 @@
 class CommentsController < ApplicationController
-  def index
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.post = @post
+
+    if @comment.save
+      flash[:success] = 'Comment saved successfully'
+      redirect_to user_post_path(@post.author_id, @post)
+    else
+      flash.now[:error] = 'Error: Comment could not be saved'
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
