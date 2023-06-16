748c586ce97184d410f367e79962a3c77956155c
Issah Mudasir <danladi496@gmail.com>
2023-06-16 02:24:45 +0000

    Fixed-specs-not-pass

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,16 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    # @posts = User.find(params[:user_id]).posts
+    @posts = Post.where(author_id: post_params[:user_id])
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by(author_id: post_params[:user_id], id: post_params[:id])
+  end
+
+  private
+
+  def post_params
+    params.permit(:user_id, :id)
   end
 end
