d32ac30c0cf1bfa93bfd76bbd217ac066d7a0448
Issah Mudasir <danladi496@gmail.com>
2023-06-16 01:46:03 +0000

    users-spec-added

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/users/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before(:example) { get users_path }
+    it 'should return success status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'it renders index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should have a body' do
+      expect(response.body).to include('<h1>User Details</h1>')
     end
   end
 
+  describe 'GET /show' do
+    before(:example) { get('/users/1') }
+    it 'should return success status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'it renders index template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should have a body' do
+      expect(response.body).to include('<h1>Show user details</h1>')
+    end
+  end
 end
