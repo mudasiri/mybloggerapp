3aedb40973f8ee9df04f8f5c2b71bf72a607b04d
Issah Mudasir <danladi496@gmail.com>
2023-06-23 15:29:56 +0000

    Update-controllers-and-views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login', sign_out: 'logout',
+    password: 'secret', confirmation: 'verification',
+    registration: 'register', edit: 'edit/profile'
+  }, sign_out_via: [:get, :post]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+
+  # root "users#home"
+
+  root 'users#index'
 
   resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show, :new, :create] do 
--- spec/views/users_show_spec.rb
+++ spec/views/users_show_spec.rb
@@ -9,8 +9,8 @@ RSpec.feature 'User show page', type: :feature do
 
   scenario 'Displays details and posts  for a single user' do
     sleep(1)
-    expect(page).to have_content('Mudasir')
-    expect(page).to have_css("img[src='https://avatars.githubusercontent.com/u/15003328?v=4']")
+    expect(page).to have_content('Mancoba')
+    expect(page).to have_css("img[src='https://avatars.githubusercontent.com/u/53082877?s=96&v=4']")
     expect(page).to have_css('li.user div.post-counter-details p', text: 'Number of posts: 5')
     expect(page).to have_content('Teacher from Mexico.')
     expect(page).to have_css('li.user', count: 1)
@@ -26,8 +26,8 @@ RSpec.feature 'User show page', type: :feature do
   end
 
   scenario 'Clicking on a user redirects to their same page' do
-    user = User.find_by(name: 'Mudasir')
-    click_on 'Mudasir'
+    user = User.find_by(name: 'Mancoba')
+    click_on 'Mancoba'
     expect(current_path).to eq(user_path(user.id))
   end
 
