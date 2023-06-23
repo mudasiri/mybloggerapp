c2d7a0aa8fe8102d9989c7bae76289cfd810e593
Issah Mudasir <danladi496@gmail.com>
2023-06-23 21:38:01 +0000

    Updated-views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,14 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password) }
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password, :role) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+      u.permit(:name, :photo, :bio, :email, :password, :current_password, :role)
     end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,9 +4,15 @@ class Comment < ApplicationRecord
 
   after_create :increment_post_comments_counter
 
+  after_destroy :decrement_post_comments_counter
+
   private
 
   def increment_post_comments_counter
     post.increment!(:comments_counter)
+  end
+
+  def decrement_post_comments_counter
+    post.decrement!(:comments_counter)
   end
 end
