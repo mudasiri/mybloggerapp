c18a8cc5a8064c0f8880324bc7d63d878da4bb42
Issah Mudasir <danladi496@gmail.com>
2023-06-23 15:32:31 +0000

    Updated-Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,60 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-first_user = User.create(name: 'Mudasir', photo: 'https://avatars.githubusercontent.com/u/15003328?v=4', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Clement', photo: 'https://avatars.githubusercontent.com/u/15003328?v=4', bio: 'Teacher from Poland.')
-third_user = User.create(name: 'John', photo: 'https://avatars.githubusercontent.com/u/15003328?v=4', bio: 'Teacher from USA.')
+# first_user = User.create(name: 'Mancoba', photo: 'https://avatars.githubusercontent.com/u/53082877?s=96&v=4', bio: 'Teacher from Mexico.')
+# second_user = User.create(name: 'Clement', photo: 'https://avatars.githubusercontent.com/u/53082877?s=96&v=4', bio: 'Teacher from Poland.')
+# third_user = User.create(name: 'John', photo: 'https://avatars.githubusercontent.com/u/53082877?s=96&v=4', bio: 'Teacher from USA.')
 
-first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-second_post = Post.create(author: first_user, title: 'Software', text: 'This is my second post')
-third_post = Post.create(author: first_user, title: 'Hello', text: 'This is my third post')
-fourth_post = Post.create(author: first_user, title: 'Hello', text: 'This is my fourth post')
-fifth_post = Post.create(author: first_user, title: 'Hello', text: 'This is my fifth post')
+# first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+# second_post = Post.create(author: first_user, title: 'Software', text: 'This is my second post')
+# third_post = Post.create(author: first_user, title: 'Hello', text: 'This is my third post')
+# fourth_post = Post.create(author: first_user, title: 'Hello', text: 'This is my fourth post')
+# fifth_post = Post.create(author: first_user, title: 'Hello', text: 'This is my fifth post')
 
-Comment.create(post: first_post, author: first_user, text: 'Hi Tom!' )
-Comment.create(post: first_post, author: first_user, text: 'Great work!' )
-Comment.create(post: first_post, author: first_user, text: 'I like the code!' )
-Comment.create(post: first_post, author: first_user, text: 'Good review!' )
-Comment.create(post: first_post, author: first_user, text: 'Wonderful news!' )
-Comment.create(post: first_post, author: first_user, text: 'Well Done!' )
-Comment.create(post: first_post, author: first_user, text: 'Awesome!' )
+# Comment.create(post: first_post, author: first_user, text: 'Hi Tom!' )
+# Comment.create(post: first_post, author: first_user, text: 'Great work!' )
+# Comment.create(post: first_post, author: first_user, text: 'I like the code!' )
+# Comment.create(post: first_post, author: first_user, text: 'Good review!' )
+# Comment.create(post: first_post, author: first_user, text: 'Wonderful news!' )
+# Comment.create(post: first_post, author: first_user, text: 'Well Done!' )
+# Comment.create(post: first_post, author: first_user, text: 'Awesome!' )
+
+Like.destroy_all
+Comment.destroy_all
+Post.destroy_all
+
+# User seeder
+user_1 = User.first
+user_2 = User.second
+
+p "Created #{User.count} Users"
+
+# Post seeder
+post_1 = Post.create(author: user_1, title: 'Post 1', text: 'One Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod.')
+post_2 = Post.create(author: user_2, title: 'Post 2', text: 'Two Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod.')
+post_3 = Post.create(author: user_1, title: 'Post 3', text: 'Three Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod.')
+post_4 = Post.create(author: user_2, title: 'Post 4', text: 'Four Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod.')
+
+p "Created #{Post.count} Posts"
+
+# Comment seeder
+Comment.create([
+{ post: post_1, author: user_1, text: 'User 1 comment in post 1' },
+{ post: post_2, author: user_2, text: 'User 2 comment in post 2' },
+{ post: post_3, author: user_1, text: 'User 1 comment in post 3' },
+{ post: post_4, author: user_2, text: 'User 2 comment in post 4' },
+{ post: post_1, author: user_1, text: 'User 1 comment in post 1' },
+{ post: post_2, author: user_2, text: 'User 2 comment in post 2' }
+])
+
+p "Created #{Comment.count} Comments"
+
+# Like seeder
+Like.create([
+{ post: post_1, author: user_1 },
+{ post: post_2, author: user_2 },
+{ post: post_3, author: user_1 },
+{ post: post_4, author: user_2 }
+])
+
+p "Created #{Like.count} Likes" 
