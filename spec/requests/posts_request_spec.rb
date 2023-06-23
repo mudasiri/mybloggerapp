924c86c2feb3d3f9898078f4705ff46190766d88
Issah Mudasir <danladi496@gmail.com>
2023-06-23 11:09:56 +0000

    Fixed-errors-inspec-integration-test

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    before(:example) { get('/users/96/posts') }
+    before(:example) { get('/users/1/posts') }
     it 'should return success status' do
       expect(response).to have_http_status(:ok)
     end
@@ -17,7 +17,7 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'GET /show' do
-    before(:example) { get('/users/96/posts/108') }
+    before(:example) { get('/users/1/posts/1') }
     it 'should return success status' do
       expect(response).to have_http_status(:ok)
     end
