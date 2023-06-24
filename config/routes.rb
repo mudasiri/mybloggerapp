f90a6fe17ebdc9a83df38a1fe02394e7be9e4c67
Issah Mudasir <danladi496@gmail.com>
2023-06-24 12:42:57 +0000

    Modified-routes

---
+++ config/routes.rb
@@ -0,0 +1,44 @@
+Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login', sign_out: 'logout',
+    password: 'secret', confirmation: 'verification',
+    registration: 'register', edit: 'edit/profile'
+  }, sign_out_via: [:get, :post]
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+
+  # root "users#home"
+
+  root 'users#index'
+
+  # resources :users, only: [:index, :show] do 
+  #   resources :posts, only: [:index, :show, :new, :create, :destroy] do 
+  #     resources :comments, only: [:new, :create, :destroy]
+  #     resources :likes, only: [:index, :create]
+  #   end
+  # end
+
+  resources :users, only: [:index, :show] do 
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do 
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:index, :create]
+    end
+  end
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do 
+        resources :posts, only: [:index, :show, :new, :create, :destroy] do 
+          resources :comments, only: [:new, :create, :destroy]
+          resources :likes, only: [:index, :create]
+        end
+      end
+    end
+  end
+
+end
